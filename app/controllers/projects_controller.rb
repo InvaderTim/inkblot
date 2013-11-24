28f1d268aaa0ae1650733af3535d1e6121bb6c96
Courtzie Wederell <courtziejayne@gmail.com>
2013-11-24 19:14:48 +1300

    initial projects feature

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,38 @@
+class ProjectsController < ApplicationController
+  def index
+  	@projects = Project.all
+  end
+  def new
+  	@project = Project.new
+  end
+  def create 
+  	@project = Project.new(project_params)
+  	if @project.save
+  		redirect_to @project
+  	else
+  		render 'new'
+  	end
+
+  end
+    def show
+	@project = Project.find(params[:id])  	
+  end
+def edit
+	@project = Project.find(params[:id])
+end
+def update
+	@project = Project.find(params[:id])
+	if @project.update(params[:project].permit(:title, :text, :genre, :rating, :language))
+		redirect_to @project
+		else
+			render 'edit'
+		end
+	end
+
+  private
+  def project_params
+  	params.require(:project).permit(:title, :text, :genre, :rating, :language)
+  	
+  end
+
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ActiveRecord::Base
+	cattr_accessor :genres do
+    ['Science Fiction','Romance', "Fantasy", "Humour", "Mystery", "Paranormal", "Thriller", "Horror", "Action", "Adventure", "Historical Fiction", "Teen Fiction", "Fan Fiction", "Poetry", "Short Story","Chick Lit", "Spiritual","Non Fiction","Biography","Essay", "Young Adult" ].sort.push('Other')
+  end
+  cattr_accessor :ratings do
+  	['G: General Audiences', 'PG: Parental Guidance Suggested', 'PG-13: Parents Strongly Cautioned', 'R: Restricted', 'NC-17']
+  end
+  validates :title, presence: true, length: { minimum: 1}
+  validates :text, presence: true, length: {minimum: 1}
 end
